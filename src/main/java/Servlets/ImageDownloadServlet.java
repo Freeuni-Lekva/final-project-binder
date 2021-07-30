60c32f1d596599f220b3e8b89a5ead3c63364fda
guramiabramishvili <gurami.abramishvili@gmail.com>
2021-07-31 01:35:14 +0400

    Image upload servlet added

---
+++ src/main/java/Servlets/ImageDownloadServlet.java
@@ -0,0 +1,46 @@
+package Servlets;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.List;
+import org.apache.commons.io.FileUtils;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+@MultipartConfig
+@WebServlet(name = "UploadDownloadFileServlet",value = "/UploadDownloadFileServlet")
+public class ImageDownloadServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if(!ServletFileUpload.isMultipartContent(request)){
+            throw new ServletException("Content type is not multipart/form-data");
+        }
+        Part filePart = request.getPart("fileName");
+        String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
+        InputStream fileContent = filePart.getInputStream();
+        File targetFile = new File("./User_Files/" + fileName);
+        FileUtils.copyInputStreamToFile(fileContent, targetFile);
+    }
+
+}
--- target/classes/DatabaseSQL/Chat_RoomSQL.sql
+++
@@ -1,7 +0,0 @@
-Create table Chat_Room(
-    chat_room_id int auto_increment primary key,
-    actions_id_1 int ,
-    actions_id_2 int ,
-    foreign key  (actions_id_1) references actions(actions_id) on update cascade on delete cascade,
-    foreign key  (actions_id_2) references actions(actions_id) on update cascade on delete cascade
-);
